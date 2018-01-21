cc04ed872801aa2fee5b417cf167f936deb1d5d9
liudehui <huideliu@163.com>
2018-01-21 23:21:46 +0800

    校验逻辑

--- src/main/java/easy/code/common/RuleUtils.java
+++ src/main/java/easy/code/common/RuleUtils.java
@@ -10,7 +10,12 @@ public final class RuleUtils {
     /**
      * 执行类 集合，可通过注入方式注入
      */
-    private final static List<IExecuteType> EXECUTE_CLASSES = new ArrayList<IExecuteType>();
+    private final static List<IExecuteType> EXECUTE_CHECKS = new ArrayList<IExecuteType>();
+    /**
+     * 是否进行 执行类型校验
+     * 若注入执行类型，就进行校验，没有注入就不用校验
+     */
+    private static boolean UN_CHECK = true;
     /**
      * 默认执行类型
      */
@@ -21,7 +26,8 @@ public final class RuleUtils {
     }
 
     public void init(List<IExecuteType> _classes) {
-        EXECUTE_CLASSES.addAll(_classes);
+        EXECUTE_CHECKS.addAll(_classes);
+        UN_CHECK = false;
     }
 
     public static RuleResult executeRule(IRuleKey ruleKey, RuleParam ruleParam) {
@@ -37,5 +43,10 @@ public final class RuleUtils {
         //打印结束日志
         return result;
 
+    }
+
+
+    private static boolean check(IExecuteType executeType) {
+        return UN_CHECK || EXECUTE_CHECKS.contains(executeType);
     }
 }
